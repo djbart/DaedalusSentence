0085510eb30722f4b7db12e764132cf3597d14ff
Bart Waeterschoot <bart.waeterschoot@gmail.com>
2015-12-21 10:17:44 +0100

    Added additional screens & segues

--- Main.swift
+++ Main.swift
@@ -97,6 +97,9 @@ class Main: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    override func preferredStatusBarStyle() -> UIStatusBarStyle {
+        return UIStatusBarStyle.LightContent
+    }
 
     /*
     // MARK: - Navigation
